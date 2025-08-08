648027969c593f84ef89f5c38e951e58c133a9ab
namsontrandinh <namson12a0111@gmail.com>
2025-08-08 16:53:18 +0700

    sontdn - camunda

---
+++ backend/src/main/java/com/demo/controller/ProcessController.java
@@ -0,0 +1,43 @@
+package com.demo.controller;
+
+import com.demo.dto.StartInstanceRequest;
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/process")
+public class ProcessController {
+    private final ZeebeClient zeebeClient;
+
+    public ProcessController(ZeebeClient zeebeClient) {
+        this.zeebeClient = zeebeClient;
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<String> startProcessInstance(@RequestBody StartInstanceRequest request) {
+        try {
+            Map<String, Object> variables = Map.of("applicationId", request.getApplicationId());
+
+            ProcessInstanceEvent instanceEvent = zeebeClient
+                    .newCreateInstanceCommand()
+                    .bpmnProcessId(request.getProcessId())
+                    .latestVersion()
+                    .variables(variables)
+                    .send()
+                    .join();
+
+            return ResponseEntity.ok("Started process instance with key: " + instanceEvent.getProcessInstanceKey());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to start process: " + e.getMessage());
+        }
+    }
+}
---
+++ backend/src/main/java/com/demo/dto/StartInstanceRequest.java
@@ -0,0 +1,22 @@
+package com.demo.dto;
+
+public class StartInstanceRequest {
+    private String processId;
+    private String applicationId;
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId;
+    }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
+}
