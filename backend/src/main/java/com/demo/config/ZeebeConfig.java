84cfe589d7a698d53c619d136ad3f59f6e673015
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2025-08-11 19:06:58 +0700

    dockerization

--- backend/src/main/java/com/demo/config/ZeebeConfig.java
+++ backend/src/main/java/com/demo/config/ZeebeConfig.java
@@ -1,16 +1,16 @@
-package com.demo.config;
-
-import io.camunda.zeebe.client.ZeebeClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ZeebeConfig {
-        @Bean
-        public ZeebeClient zeebeClient() {
-            return ZeebeClient.newClientBuilder()
-                    .gatewayAddress("localhost:26500")
-                    .usePlaintext()
-                    .build();
-        }
-}
+//package com.demo.config;
+//
+//import io.camunda.zeebe.client.ZeebeClient;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ZeebeConfig {
+//        @Bean
+//        public ZeebeClient zeebeClient() {
+//            return ZeebeClient.newClientBuilder()
+//                    .gatewayAddress("localhost:26500")
+//                    .usePlaintext()
+//                    .build();
+//        }
+//}
